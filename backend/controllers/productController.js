72af1e12c4e9f520d470ebc166c76be24dcb13d2
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-08 17:46:44 -0400

    adding create product endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,40 @@
+const Product = require('../models/product')
+
+// Create new product   =>   /api/v1/admin/product/new
+exports.newProduct = async (req, res, next) => {
+
+    // let images = []
+    // if (typeof req.body.images === 'string') {
+    //     images.push(req.body.images)
+    // } else {
+    //     images = req.body.images
+    // }
+
+    // let imagesLinks = [];
+
+    // for (let i = 0; i < images.length; i++) {
+    //     const result = await cloudinary.v2.uploader.upload(images[i], {
+    //         folder: 'products'
+    //     });
+
+    //     imagesLinks.push({
+    //         public_id: result.public_id,
+    //         url: result.secure_url
+    //     })
+    // }
+
+    // req.body.images = imagesLinks
+    // req.body.user = req.user.id;
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
