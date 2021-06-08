5a4d27c49ed75affc42fa9fcd68e3732faabb170
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-08 12:01:32 -0400

    adding first generic route

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+// Import all routes
+const products = require('./routes/products');
+
+app.use('/api/v1', products);
+
+module.exports = app;
