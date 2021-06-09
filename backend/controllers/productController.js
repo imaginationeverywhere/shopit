9729b8758487d4a8ba7f314ea25790bd83c5f14c
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-09 08:18:58 -0400

    added search by keyword name functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures')
 
 // Create new product   =>   /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -39,7 +40,10 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 // Get all products => /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
-    const products = await Product.find();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+    
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
