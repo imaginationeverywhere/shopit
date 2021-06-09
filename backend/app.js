c8f57610dc52c519032f98a95e74dd39bea5b6c6
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-08 23:04:09 -0400

    added error handling for production and development

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 const errorMiddleware = require('./middlewares/errors')
 
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Import all routes
 const products = require('./routes/products');
