4aa1386e5f51c041217ac3604e5bed05c680ca6d
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-08 21:08:37 -0400

    added get single product route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -45,3 +45,27 @@ exports.getProducts = async (req, res, next) => {
         products
     })
 }
+
+// Get single product details   =>   /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    // if (!product) {
+    //     return next(new ErrorHandler('Product not found', 404));
+    // }
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+}
