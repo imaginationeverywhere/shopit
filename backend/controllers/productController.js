b0d8df333f0b046b1f0a960aff588f6c18cc4f0c
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-08 21:21:04 -0400

    added update product route and controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -69,3 +69,65 @@ exports.getSingleProduct = async (req, res, next) => {
     })
 
 }
+
+// Update Product   =>   /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    // if (!product) {
+    //     return next(new ErrorHandler('Product not found', 404));
+    // }
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    // let images = []
+    // if (typeof req.body.images === 'string') {
+    //     images.push(req.body.images)
+    // } else {
+    //     images = req.body.images
+    // }
+
+    // if (images !== undefined) {
+
+    //     // Deleting images associated with the product
+    //     for (let i = 0; i < product.images.length; i++) {
+    //         const result = await cloudinary.v2.uploader.destroy(product.images[i].public_id)
+    //     }
+
+    //     let imagesLinks = [];
+
+    //     for (let i = 0; i < images.length; i++) {
+    //         const result = await cloudinary.v2.uploader.upload(images[i], {
+    //             folder: 'products'
+    //         });
+
+    //         imagesLinks.push({
+    //             public_id: result.public_id,
+    //             url: result.secure_url
+    //         })
+    //     }
+
+    //     req.body.images = imagesLinks
+
+    // }
+
+
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+}
