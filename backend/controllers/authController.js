1173653b197391676e148de635c35250770441c4
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-10 20:32:43 -0400

    added jwt token and encrypted password

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -31,37 +31,46 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
     // sendToken(user, 200, res)
 
+    const token = user.getJwtToken();
+
     res.status(201).json({
         success:true,
-        user
+        token
     })
 })
 
 // Login User  =>  /a[i/v1/login
-// exports.loginUser = catchAsyncErrors(async (req, res, next) => {
-//     const { email, password } = req.body;
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
 
-//     // Checks if email and password is entered by user
-//     if (!email || !password) {
-//         return next(new ErrorHandler('Please enter email & password', 400))
-//     }
+    // Checks if email and password is entered by user
+    if (!email || !password) {
+        return next(new ErrorHandler('Please enter email & password', 400))
+    }
 
-//     // Finding user in database
-//     const user = await User.findOne({ email }).select('+password')
+    // Finding user in database
+    const user = await User.findOne({ email }).select('+password')
 
-//     if (!user) {
-//         return next(new ErrorHandler('Invalid Email or Password', 401));
-//     }
+    if (!user) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
 
-//     // Checks if password is correct or not
-//     const isPasswordMatched = await user.comparePassword(password);
+    // Checks if password is correct or not
+    const isPasswordMatched = await user.comparePassword(password);
 
-//     if (!isPasswordMatched) {
-//         return next(new ErrorHandler('Invalid Email or Password', 401));
-//     }
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
 
-//     sendToken(user, 200, res)
-// })
+    const token = user.getJwtToken();
+
+    res.status(200).json({
+        success:true,
+        token
+    })
+
+    // sendToken(user, 200, res)
+})
 
 // Forgot Password   =>  /api/v1/password/forgot
 // exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
     registerUser,
+    loginUser,
 } = require('../controllers/authController');
 
 router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
 
 module.exports = router;
