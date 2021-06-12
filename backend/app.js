f1225ffbde17c44e9d56034a2b2293c5ac7d3abd
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-12 02:32:58 -0400

    implemented order functionality

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,11 @@ app.use(cookieParser())
 // Import all routes
 const products = require('./routes/products');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth)
+app.use('/api/v1', order)
 
 // Middleware to handle errors
 app.use(errorMiddleware);
