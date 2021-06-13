a0ea536c4b1c33081420cd8b85fd607cacc0b11d
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-13 13:04:47 -0400

    implemented registration reducer and action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,42 +1,72 @@
 import {
-    CLEAR_ERRORS,
-    LOGIN_FAIL,
-    LOGIN_REQUEST,
-    LOGIN_SUCCESS
-} from '../constants/userConstants'
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+  CLEAR_ERRORS,
+} from '../constants/userConstants';
 
-import axios from 'axios'
+import axios from 'axios';
 
 // Login
 export const login = (email, password) => async (dispatch) => {
-    try {
+  try {
+    dispatch({ type: LOGIN_REQUEST });
 
-        dispatch({ type: LOGIN_REQUEST })
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
+    const { data } = await axios.post(
+      '/api/v1/login',
+      { email, password },
+      config
+    );
 
-        const { data } = await axios.post('/api/v1/login', { email, password }, config)
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGIN_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
-        dispatch({
-            type: LOGIN_SUCCESS,
-            payload: data.user
-        })
+// Register user
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST });
 
-    } catch (error) {
-        dispatch({
-            type: LOGIN_FAIL,
-            payload: error.response.data.message
-        })
-    }
-}
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+
+    const { data } = await axios.post('/api/v1/register', userData, config);
+
+    dispatch({
+      type: REGISTER_USER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
 
 // Clear Errors
 export const clearErrors = () => async (dispatch) => {
-    dispatch({
-        type: CLEAR_ERRORS
-    })
-}
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
