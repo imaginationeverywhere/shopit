658b6a612a110ab584925d28691998a780c338a4
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-13 12:18:04 -0400

    implemented login reducer and action

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,36 @@
+import {
+    CLEAR_ERRORS,
+    LOGIN_FAIL,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS
+} from '../constants/userConstants'
+
+import axios from 'axios'
+
+// Login
+export const login = (email, password) => async (dispatch) => {
+    try {
+
+        dispatch({ type: LOGIN_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post('/api/v1/login', { email, password }, config)
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: data.user
+        })
+
+    } catch (error) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,26 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { productDetailsReducer, productsReducer } from './reducers/productReducers'
+import {
+  productDetailsReducer,
+  productsReducer,
+} from './reducers/productReducers';
 
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { authReducer } from './reducers/userReducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
-    products: productsReducer,
-    productDetails: productDetailsReducer,
-})
+  products: productsReducer,
+  productDetails: productDetailsReducer,
+  auth: authReducer,
+});
 
-
-let initialState = {}
+let initialState = {};
 
 const middleware = [thunk];
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
