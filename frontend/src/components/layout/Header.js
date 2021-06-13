4a4d3dd37236e56d96caf7ba795d8d7d00fc7898
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-13 15:33:29 -0400

    implemented the dropdown menu in the header bar

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,9 +1,17 @@
 import { Link, Route } from 'react-router-dom';
 import React, { Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Search from './Search'
+import { useAlert } from 'react-alert';
+
+import Search from './Search';
 
 const Header = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector((state) => state.auth);
+
   return (
     <Fragment>
       <nav className='navbar row'>
@@ -22,16 +30,66 @@ const Header = () => {
         </div>
 
         <div className='col-12 col-md-3 mt-4 mt-md-0 text-center'>
-          <Link to="/login" className='btn' id='login_btn'>
-            Login
+          <Link to='/cart' style={{ textDecoration: 'none' }}>
+            <span id='cart' className='ml-3'>
+              Cart
+            </span>
+            <span className='ml-1' id='cart_count'>
+              2
+            </span>
           </Link>
+          {user ? (
+            <div className='ml-4 dropdown d-inline'>
+              <Link
+                to='#!'
+                className='btn dropdown-toggle text-white mr-4'
+                type='button'
+                id='dropDownMenuButton'
+                data-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                <figure className='avatar avatar-nav'>
+                  <img
+                    src={user.avatar && user.avatar.url}
+                    alt={user && user.name}
+                    className='rounded-circle'
+                  />
+                </figure>
+                <span>{user && user.name}</span>
+              </Link>
 
-          <span id='cart' className='ml-3'>
-            Cart
-          </span>
-          <span className='ml-1' id='cart_count'>
-            2
-          </span>
+              <div
+                className='dropdown-menu'
+                aria-labelledby='dropDownMenuButton'
+              >
+                {user && user.role === 'admin' && (
+                  <Link className='dropdown-item' to='/dashboard'>
+                    Dashboard
+                  </Link>
+                )}
+                <Link className='dropdown-item' to='/orders/me'>
+                  Orders
+                </Link>
+                <Link className='dropdown-item' to='/me'>
+                  Profile
+                </Link>
+                <Link
+                  className='dropdown-item text-danger'
+                  to='/'
+                  // onClick={logoutHandler}
+                >
+                  Logout
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link to='/login' className='btn ml-4' id='login_btn'>
+                Login
+              </Link>
+            )
+          )}
         </div>
       </nav>
     </Fragment>
