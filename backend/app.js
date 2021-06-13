7f3e58ac259f5382bebdac59ed409f2b01f9af8d
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-13 13:32:30 -0400

    add file upload for images

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,14 @@ const app = express();
 
 const cookieParser = require('cookie-parser')
 const errorMiddleware = require('./middlewares/errors')
+const fileUpload = require('express-fileupload')
 
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
   }));
 app.use(cookieParser())
+app.use(fileUpload());
 
 
 // Import all routes
@@ -19,6 +21,14 @@ const order = require('./routes/order');
 app.use('/api/v1', products);
 app.use('/api/v1', auth)
 app.use('/api/v1', order)
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 // Middleware to handle errors
 app.use(errorMiddleware);
