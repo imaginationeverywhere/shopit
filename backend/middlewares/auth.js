d6f0c7ac556ba31b4a95a2af4b48b19a316e82dd
Chinoso Jude <chinosojude@Chinosos-MacBook-Pro.local>
2021-12-27 10:17:44 +0100

    fix start bug

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,14 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
-const jwt = require("jsonwebtoken");
-const ErrorHandler = require("../utils/errorHandler");
-const catchAsyncErrors = require("./catchAsyncErrors");
+const jwt = require('jsonwebtoken');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('./catchAsyncErrors');
 
 // Checks if user is authenticated or not
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
-    return next(new ErrorHandler("Login first to access this resource.", 401));
+    return next(new ErrorHandler('Login first to access this resource.', 401));
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -21,11 +21,11 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
 exports.authorizeRoles = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-s      return next(
+      return next(
         new ErrorHandler(
           `Role (${req.user.role}) is not allowed to acccess this resource`,
-          403
-        )
+          403,
+        ),
       );
     }
     next();
